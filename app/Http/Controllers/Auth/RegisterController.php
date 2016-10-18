2075dfafb31c30cba3dc248eaf5dd6fe7017eb68
Harish <haristoshniwal@gmail.com>
2016-10-18 19:01:34 +0530

    laravel 5.3 install and email verification added

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use DB;
+use Mail;
+use App\User;
+use Validator;
+use Illuminate\Http\Request;
+use App\Mail\EmailVerification;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\RegistersUsers;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after login / registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+            'email_token' => str_random(10),
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        // Laravel validation
+        $validator = $this->validator($request->all());
+        if ($validator->fails()) 
+        {
+            $this->throwValidationException($request, $validator);
+        }
+        // Using database transactions is useful here because stuff happening is actually a transaction
+        // I don't know what I said in the last line! Weird!
+        DB::beginTransaction();
+        try
+        {
+            $user = $this->create($request->all());
+            // After creating the user send an email with the random token generated in the create method above
+            $email = new EmailVerification(new User(['email_token' => $user->email_token]));
+            Mail::to($user->email)->send($email);
+            DB::commit();
+            return back();
+        }
+        catch(Exception $e)
+        {
+            DB::rollback(); 
+            return back();
+        }
+    }
+
+    public function verify($token)
+    {
+        // The verified method has been added to the user model and chained here
+        // for better readability
+        User::where('email_token',$token)->firstOrFail()->verified();
+        return redirect('login');
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+
+ **Laravel 5.3.19 app boilerplate with email verification on registration**
+
+Every time I or my colleagues start a laravel project , we need to re-implement this process of adding email verification in the project every time , so created this boilerplate out of personal needs 
+
+How I achieved this can be read [here at my blog](http://www.lubus.in/blog/adding-email-verification-in-laravel-5-3-app-149)
+
+ - After installation , configure the .env file (App key, Database credentials, Mail driver)
+ - Run `php artisan migrate`
+ - That's it , you are done!
+
+I advise you to read the blog mentioned above to understand what's going on here exactly. If you have any questions or suggestions , drop a comment on the blog or create an issue here , would be happy to help , or just come say hi :-) 
+
+> Will keep it updated with the latest stable laravel version
